68aa25a4b27019239a3438d5f87e043de8944703
Michael Bleistein <michael.bleistein@microfocus.com>
2018-04-15 17:51:12 -0400

    Test Case added

---
+++ TestLoanAgileVS/TestLOANAMORT.cbl
@@ -0,0 +1,80 @@
+      *> Test Fixture for LoanAgileVS, LOANAMORT
+
+       copy "mfunit_prototypes.cpy".
+
+       program-id. TestLOANAMORT.
+       working-storage section.
+       copy "mfunit.cpy".
+
+       01 WS-FAIL-MSG.
+          03 FILLER      PIC X(31) VALUE "EXPECTED Total Interest value: ".
+          03 WK-EXPECTED-VALUE     PIC X(09).
+          03 FILLER      PIC X(31) VALUE "  ACTUAL Total Interest value: ".
+          03 WK-ACTUAL-VALUE       PIC X(09).
+
+       78 TEST-TESTLOANAMORT value "TestLOANAMORT".
+       01 pp procedure-pointer.
+
+      *> Program linkage data
+       01 LOANINFO.
+         03 PRINCIPAL PIC S9(8) COMP-3.
+         03 LOANTERM PIC S9(8) COMP-3.
+         03 RATE PIC S9(9)V9(9).
+       01 OUTDATA.
+         03 PAYMENTS occurs 480 depending on LOANTERM.
+           05 OUTINTPAID PIC $$,$(3).99.
+           05 OUTPRINCPAID PIC $$,$(3).99.
+           05 OUTPAYMENT PIC $$,$(3).99.
+           05 OUTBALANCE PIC $(3),$(3).99.
+         03 OUTTOTINTPAID PIC $$,$(3).99.
+
+       procedure division.
+           goback returning 0
+       .
+
+       entry MFU-TC-PREFIX & TEST-TESTLOANAMORT.
+
+		   move 36000 to PRINCIPAL
+		   move 4.5   to RATE
+		   move 48    to LOANTERM
+
+           call "LOANAMORT" using
+                       by reference LOANINFO
+                       by reference OUTDATA
+
+		   IF OUTINTPAID(1) =   "  $135.00" and
+			  OUTPRINCPAID(1) = "  $685.92" and
+			  OUTPAYMENT(1)   = "  $820.92" and
+			  OUTBALANCE(1)   = "$35,314.00" and
+			  OUTTOTINTPAID   = "$3,404.60"
+			   continue
+	       else 
+              move OUTTOTINTPAID to WK-ACTUAL-VALUE
+			  move "$3,404.60"   to WK-EXPECTED-VALUE
+              CALL "MFU_ASSERT_FAIL" USING
+                 BY REFERENCE WS-FAIL-MSG
+                 BY VALUE 80
+           END-IF
+
+       
+
+
+           *> Verify the outputs here
+           goback returning MFU-PASS-RETURN-CODE
+       .
+
+      $region TestCase Configuration
+
+       entry MFU-TC-SETUP-PREFIX & TEST-TESTLOANAMORT.
+           *> Load the library that is being tested
+           set pp to entry "LoanAgileVS"
+
+           initialize LOANINFO
+           initialize OUTDATA
+           *> Add any other test setup code here
+           goback returning 0
+       .
+
+      $end-region
+
+       end program.
