f84693318dab4c36b901670138be568114e73029
Michael Bleistein <michael.bleistein@microfocus.com>
2018-10-30 14:58:34 -0400

    Added Docker .bat files

---
+++ bld_and_mfurun.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+set WORK_DIR=c:\work
+echo.
+echo Compiling:
+msbuild LoanAgileVS.sln /p:OutDir=%WORK_DIR% /p:BaseIntermediateOutputPath=%WORK_DIR%\obj\ /verbosity:minimal /p:Configuration=Release
+if errorlevel 1 goto theend
+
+cd %WORK_DIR%
+echo Running tests:
+mfurun -report:markdown -report:junit -jenkins-ci -jit:nodebug -timeout:10s -sp -outdir:results TestLoanAgileVS.mfu
+echo.
+echo Moving test results out of the container.
+move results\*.* %RESULTS_DIR% >nul
+if errorlevel 1 echo WARNING: move results failed
+
+:theend
---
+++ docker-mfurun.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal EnableDelayedExpansion
+rem set then environment variables in bld.env
+for /F %%a in (bld.env) do set %%a
+
+rem the 'src' directory is mapped to c:\'src' directory in the container, 
+rem same for the results directory
+set "SHARE_SRC_ARG=-v %CD%:c:\%SRC%"
+set "SHARE_RES_ARG=-v %CD%\results:c:\results -e RESULTS_DIR=c:\results"
+rem does the base image exist?
+MBdocker inspect --format='{{.Config.Image}}' %IMAGE% >nul 2>&1
+if errorlevel 1 (
+	echo ERROR: Aborting, the base %IMAGE% is not present
+	goto theend)
+
+@if not exist results mkdir results
+
+echo Creating docker container using %IMAGE%
+echo  Directory %SRC% is shared to the container
+echo  Directory results contains the results
+MBdocker run --rm %SHARE_SRC_ARG% %SHARE_RES_ARG% --workdir c:\src %IMAGE% cmd.exe /c bld_and_mfurun.bat
+
+echo.
+echo Test results can be found in results dir:
+dir results
+:theend
